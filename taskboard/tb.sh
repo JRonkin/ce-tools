5f3d9c258a7ecc360c8268f8493198c873210651
jronkin <jronkin@yext.com>
2018-08-07 22:26:41 -0400

    Implemented selection UI

--- taskboard/tb.sh
+++ taskboard/tb.sh
@@ -1,43 +1,58 @@
+tasks=()
 selection=0
+active=-1
 
 while :
 do
 	clear
 	printf "\
-======================================================
-| Q: Quit QS  | N: New      | M: Minimize | X: Close |
-| [Enter]: Switch to Selection                       |
-|----------------------------------------------------|
-======================================================
+=========================================================
+| Q: Quit     | N: New      | M: Minimize | X: Close    |
+| [Enter]: Switch to Selection                          |
+|-------------------------------------------------------|
+"
+for ((i = 0; i < ${#tasks[@]}; i++))
+do
+	if [[ $selection = $i ]]; then s=">"; else s=" "; fi
+	if [[ $active = $i ]]; then a="*"; else a=" "; fi
+	printf "\
+| $s$a%s |
+" "$(echo "${tasks[i]}                                                   " | sed "s/\(.\{51\}\).*/\1/")"
+done
+	printf "\
+=========================================================
 "
 
 	read -n 1 input
 	case "$( echo $input | tr a-z A-Z )" in
 
 		"Q" )
+			clear
 			break
 			;;
 
 		"N" )
 			clear
-			read -p "Git Clone URL (SSH): " giturl
-			if [[ giturl =~ "git@github\.com:.+/(.+)\.git" ]]
+			read -p "GitHub URL: " giturl
+			if [[ "$giturl" =~ .*github\.com\/[^/]+\/([^/]+).* ]]
 			then
 				repo=${BASH_REMATCH[1]}
 			else
-				echo "Invalid URL"
+				printf "Invalid URL:\n$giturl\n\n> Return to TaskBoard"
 				read -n 1
 				continue
 			fi
 			read -p "JIRA Item URL: " jiraurl
-			if [[ jiraurl =~ ".*selectedIssue=([^\&]+).*" ]]
+			if [[ "$jiraurl" =~ .*selectedIssue=([^\&]+).* ]]
 			then
 				jiranum=${BASH_REMATCH[1]}
 			else
-				echo "No JIRA number found -- please select an issue and copy the URL."
+				printf "No JIRA number found -- select an issue and copy the URL\n\n> Return to TaskBoard"
 				read -n 1
 				continue
 			fi
+			selection=${#tasks[*]}
+			tasks[${#tasks[*]}]="$jiranum   $repo"
 			./quickswap.sh -n giturl jiraurl
 			;;
 
@@ -45,12 +60,26 @@ do
 			read -n 2 -t 1 input2
 			case $input2 in
 				"[A" )
-					echo "UP"
+					if [[ $selection -gt 0 ]]
+					then
+						selection=$(( $selection - 1 ))
+					else
+						selection=$(( ${#tasks[*]} - 1 ))
+					fi
 					;;
 				"[B" )
-					echo "DOWN"
+					if [[ $selection -lt $(( ${#tasks[*]} - 1 )) ]]
+					then
+						selection=$(( $selection + 1 ))
+					else
+						selection=0
+					fi
 					;;
 			esac
+			;;
+
+		"" )
+			active=$selection
 			;;
 
 	esac
