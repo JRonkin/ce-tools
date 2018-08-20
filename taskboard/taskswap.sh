118efbb1ae08799cd366e5b8c79b7085a345a534
jronkin <jronkin@yext.com>
2018-08-20 12:33:17 -0400

    Fixed "new" function

--- taskboard/taskswap.sh
+++ taskboard/taskswap.sh
@@ -2,30 +2,31 @@ new() {
 	repo=$1
 	jiranum=$2
 
-	killall -9 grunt
+	killall -9 grunt || true
 
 	cd ~/repo
-	git clone $repo || true
+	echo "Cloning git@github.com:yext-pages/${repo}.git"
+	git clone "git@github.com:yext-pages/${repo}.git" || true
 
 	printf 'tell app "Terminal"
-				do script "cd ~/repo/${repo} && git co -b ${jiranum}/trunk"
+				do script "cd ~/repo/%s && git co %s/trunk || git co -b %s/trunk"
 			end tell
-		' | osascript
+		' $repo $jiranum $jiranum | osascript
 
 	printf 'tell app "Terminal"
-				do script "cd ~/repo/${repo}/src; rm yarn.lock; yarn install; bower install; bundle install"
+				do script "cd ~/repo/%s/src && touch yarn.lock && rm yarn.lock && yarn install && bower install && bundle install"
 			end tell
-		' | osascript
+		' $repo | osascript
 }
 
 activate() {
-
+	repo=$1
 }
 
 deactivate() {
-
+	repo=$1
 }
 
 close() {
-
+	repo=$1
 }
