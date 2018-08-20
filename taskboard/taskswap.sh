930a7050f001ea3baadb32865a19d7a39b1f25b3
jronkin <jronkin@yext.com>
2018-08-20 16:23:53 -0400

    Added activate, deactivate, and close

--- taskboard/taskswap.sh
+++ taskboard/taskswap.sh
@@ -7,25 +7,42 @@ new() {
 
 	printf 'tell app "Terminal"
 				do script "cd ~/repo/%s && atom . && git co %s/trunk || git co -b %s/trunk"
+				set the custom title of the front window to %s
 				set the bounds of the front window to {3256, 387, 3841, 753}
 			end tell
-		' $repo $jiranum $jiranum | osascript
+		' $repo $jiranum $jiranum $repo | osascript
 
 	printf 'tell app "Terminal"
 				do script "cd ~/repo/%s/src && touch yarn.lock && rm yarn.lock && yarn install && bower install && bundle install"
+				set the custom title of the front window to %s
 				set the bounds of the front window to {3255, 754, 3840, 1120}
 			end tell
-		' $repo | osascript
+		' $repo $repo | osascript
 }
 
 activate() {
 	repo=$1
+
+	printf 'tell app "Terminal"
+				set miniaturized of every window with custom title "%s" to false
+			end tell
+		' $repo | osascript
 }
 
 deactivate() {
 	repo=$1
+
+	printf 'tell app "Terminal"
+				set miniaturized of every window with custom title "%s" to true
+			end tell
+		' $repo | osascript
 }
 
 close() {
 	repo=$1
+
+	printf 'tell app "Terminal"
+				close every window with custom title "%s"
+			end tell
+		' $repo | osascript
 }
