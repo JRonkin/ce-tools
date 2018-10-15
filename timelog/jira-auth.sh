61d7d75b3d718fcf3f7ec0f1ac492db768211698
jronkin <jronkin@yext.com>
2018-10-15 19:33:21 -0400

    fixed kira-auth token logic

--- timelog/jira-auth.sh
+++ timelog/jira-auth.sh
@@ -10,26 +10,11 @@ jira-auth() {
 		done
 	fi
 
-	apiToken="$(security find-generic-password -s "TimeLog" -a "$username" -w)"
+	savedToken="$(security find-generic-password -s "TimeLog" -a "$username" -w)"
 
-	if [ ! "$apiToken" ]
+	if [ "$2" ]
 	then
-		echo "Enter your JIRA API token for ${username}"
-		echo "If you haven't created a token yet, create one here:"
-		echo "https://id.atlassian.com/manage/api-tokens"
-
-		while [ ! "$apiToken" ]
-		do
-			read -sp "API Token: " apiToken
-			echo ""
-		done
-
-		if [[ "$(read -p "Save your token in Keychain? (y/N) ")" =~ ^[Yy]([Ee][Ss])?$ ]]
-		then
-			security add-generic-password -s "TimeLog" -a "$username" -l "JIRA API token" -p "$apiToken" -U
-		fi
-	else
-		if [ "$2" ] && [ ! "$2" = "$apiToken" ]
+		if [ "$savedToken" ] && [ ! "$2" = "$savedToken" ]
 		then
 			echo "The given API token does not match the saved token for '${username}'."
 			if [[ "$(read -p "Update saved token? (y/N) ")" =~ ^[Yy]([Ee][Ss])?$ ]]
@@ -38,6 +23,26 @@ jira-auth() {
 			fi
 
 			apiToken="$2"
+		fi
+	else
+		if [ "$savedToken" ]
+		then
+			apiToken="$savedToken"
+		else
+			echo "Enter your JIRA API token for ${username}"
+			echo "If you haven't created a token yet, create one here:"
+			echo "https://id.atlassian.com/manage/api-tokens"
+
+			while [ ! "$apiToken" ]
+			do
+				read -sp "API Token: " apiToken
+				echo ""
+			done
+
+			if [[ "$(read -p "Save your token in Keychain? (y/N) ")" =~ ^[Yy]([Ee][Ss])?$ ]]
+			then
+				security add-generic-password -s "TimeLog" -a "$username" -l "JIRA API token" -p "$apiToken" -U
+			fi
 		fi
 	fi
 }
