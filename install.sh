e8cd8b76147142c26d861d8f8d6af4be8a3bb469
jronkin <jronkin@yext.com>
2018-10-16 21:40:34 -0400

    sourcing install.sh updates PATH without needing shell restart

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 if [ ! "$(grep "# yext ce tools" < "${HOME}/.bash_profile")" ]
 then
 	cd "$(dirname "${BASH_SOURCE[0]}")"
+	export PATH="$PATH:$(pwd)/shortcuts"
 	printf "
 # yext ce tools
-export PATH=\$PATH:$(pwd)/shortcuts
+export PATH=\"\$PATH:$(pwd)/shortcuts\"
 " >> "$HOME"/.bash_profile
 fi
