bc880388942b4c78fbb74fbca875efc2b36ada59
jronkin <jronkin@yext.com>
2018-10-19 17:37:58 -0400

    added make protos to update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,4 @@
-cd $ALPHA/gocode/src/yext && git pull && glock sync yext && make binaries &
+cd $ALPHA/gocode/src/yext && git pull && glock sync yext && make protos && make binaries &
 
 brew upgrade &
 
