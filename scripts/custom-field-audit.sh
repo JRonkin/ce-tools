e20a83f3f844cce4c7714e05f1cb0a876fadcf71
jronkin <jronkin@yext.com>
2019-01-28 15:24:20 -0500

    fix custom field audit

--- scripts/custom-field-audit.sh
+++ scripts/custom-field-audit.sh
@@ -4,8 +4,32 @@ then
 	exit 1
 fi
 
-grep -R '\$customByName' src/templates |
-	sed $'s/\$customByName\?*/\\\n:/g' |
-	grep '^:' |
+soy="$(grep -R -e '[^ ]customByName' src/templates |
+	sed $'s/customByName\?*/\\\n:/g' |
+	grep -e '^:[.[]' |
 	sed 's/^:\.\([0-9A-Za-z_]*\).*/\1/g; s/^:\[\([^][]*\)\].*/\1/g; s/^:\[\(.*\)\].*/\1/g' |
-	sort -u > CustomFieldAudit.txt
+	sort -u)"
+
+js="$(grep -R 'customByName' src/js)"
+
+coffee="$(grep -R 'customByName' src/coffee)"
+
+[ -f CustomFieldAudit.txt ] && rm CustomFieldAudit.txt
+
+if [ "$soy" ]
+then
+	echo "~~~~~ SOY ~~~~~" >> CustomFieldAudit.txt
+	echo "$soy" >> CustomFieldAudit.txt
+fi
+
+if [ "$js" ]
+then
+	echo "~~~~~ JS ~~~~~" >> CustomFieldAudit.txt
+	echo "$js" >> CustomFieldAudit.txt
+fi
+
+if [ "$coffee" ]
+then
+	echo "~~~~~ COFFEE ~~~~~" >> CustomFieldAudit.txt
+	echo "$coffee" >> CustomFieldAudit.txt
+fi
