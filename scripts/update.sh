7977fe0ab8fae2e29ab422f0c487ff5126b1dbdf
jronkin <jronkin@yext.com>
2019-02-20 15:57:30 -0500

    fix update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,8 @@
-cd $YEXT && git pull && glock sync yext && make &
-
-cd $CONGO && git pull && glock sync congo && glock sync yext && make &
+(
+	cd $YEXT && git co master && git pull && glock sync yext && make
+	cd $CONGO && git co master && git pull && glock sync congo && glock sync yext && make
+	cd $ALPHA && git co master && git pull && make
+) &
 
 cd ~/repo/generator-ysp && git pull &
 
@@ -12,9 +14,10 @@ cd ~/repo/yext-ce-tools && git pull &
 
 brew upgrade &
 
-npm i -g npm &
-
-npm i -g bower &
+(
+	npm i -g npm 
+	npm i -g bower
+) &
 
 pip install --upgrade pip --user &
 
