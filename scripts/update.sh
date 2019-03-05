f6bfae946adf66927db13deb313ca4df597fe776
jronkin <jronkin@yext.com>
2019-03-05 14:41:34 -0500

    do sequential updates unless specified parallel

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,24 +1,51 @@
-(
-	cd $YEXT && git co master && git pull && glock sync yext && make
-	cd $CONGO && git co master && git pull && glock sync congo && glock sync yext && make
-	cd $ALPHA && git co master && git pull && make
-) &
+# Run updates in parallel if flag is set
+if [ "$1" = "-p" ]
+then
+	(
+		cd $YEXT && git co master && git pull && glock sync yext && make
+		cd $CONGO && git co master && git pull && glock sync congo && glock sync yext && make
+		cd $ALPHA && git co master && git pull && make
+	) &
 
-cd ~/repo/generator-ysp && git co master && git pull && yarn install && yarn link &
+	cd ~/repo/generator-ysp && git co master && git pull && yarn install && yarn link &
 
-cd ~/repo/pages-builder && git pull &
+	cd ~/repo/pages-builder && git pull &
 
-cd ~/repo/pages-tools && git pull &
+	cd ~/repo/pages-tools && git pull &
 
-brew upgrade &
+	brew upgrade &
 
-(
-	npm i -g npm 
-	npm i -g bower
-) &
+	(
+		npm i -g npm 
+		npm i -g bower
+	) &
 
-pip install --upgrade pip --user &
+	pip install --upgrade pip --user &
 
-cd ~/repo/yext-ce-tools && git pull &
+	cd ~/repo/yext-ce-tools && git pull &
 
-wait
+	wait
+else
+	(
+		cd $YEXT && git co master && git pull && glock sync yext && make
+		cd $CONGO && git co master && git pull && glock sync congo && glock sync yext && make
+		cd $ALPHA && git co master && git pull && make
+	)
+
+	cd ~/repo/generator-ysp && git co master && git pull && yarn install && yarn link
+
+	cd ~/repo/pages-builder && git pull
+
+	cd ~/repo/pages-tools && git pull
+
+	brew upgrade
+
+	(
+		npm i -g npm 
+		npm i -g bower
+	)
+
+	pip install --upgrade pip --user
+
+	cd ~/repo/yext-ce-tools && git pull
+fi
