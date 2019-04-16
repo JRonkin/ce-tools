2297d258013458b6486c87c216a178e0b38e6ccd
jronkin <jronkin@yext.com>
2019-04-16 17:59:02 -0400

    publish-build on ungrouped branch uses pgs publish without --branch

--- flow2/publish-build.sh
+++ flow2/publish-build.sh
@@ -1,4 +1,11 @@
 source "$(dirname "${BASH_SOURCE[0]}")/funcs.sh"
 assert_git_repo
 
-pgs publish --branch "$(get_current_group)/build" --noopen $@
+group="$(get_current_group 2>/dev/null)"
+
+if [ "$group" ]
+then
+	pgs publish --branch "${group}/build" --noopen $@
+else
+	pgs publish --noopen $@
+fi
