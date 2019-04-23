93e345ea65a73606bd336d2f497db08377bd2231
jronkin <jronkin@yext.com>
2019-04-23 09:47:18 -0400

    fix sites-pager-helper

--- scripts/sites-pager-helper.sh
+++ scripts/sites-pager-helper.sh
@@ -5,4 +5,4 @@ then
 	read -p "Site Domain: " domain
 fi
 
-sites-pager -m prod -d "${1}${domain}" -e staging -l "$(dirname $(pwd))" —staticdirs 'src/.tmp' —templatedir 'src/templates'
+sites-pager -m prod -d "${domain}" -e staging -l "$(dirname $(pwd))" —staticdirs 'src/.tmp' —templatedir 'src/templates'
