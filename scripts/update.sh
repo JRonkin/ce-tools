447f1491df8062964bdf3c0bec6662ad45236921
jronkin <jronkin@yext.com>
2019-04-24 15:03:57 -0400

    Remove duplicate code in update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,57 +1,52 @@
 # Run updates in parallel if flag is set
+parallel=''
 if [ "$1" = "-p" ]
 then
-	(
-		cd $YEXT && git co master && git pull && glock sync yext && make
-		cd $CONGO && git co master && git pull && glock sync congo && glock sync yext && make
-		cd $ALPHA && git co master && git pull && make
-	) &
-
-	cd ~/repo/generator-ysp && git co master && git pull && yarn install && yarn link &
-
-	cd ~/repo/pages-builder && git pull &
-
-	cd ~/repo/pages-tools && git pull &
-
-	brew upgrade &
-
-	(
-		npm i -g npm 
-		npm i -g bower
-	) &
-
-	(
-		pip install --upgrade pip --user
-		pip install --upgrade awscli --user
-	) &
-
-	cd ~/repo/yext-ce-tools && git pull &
-
-	wait
-else
-	(
-		cd $YEXT && git co master && git pull && glock sync yext && make
-		cd $CONGO && git co master && git pull && glock sync congo && glock sync yext && make
-		cd $ALPHA && git co master && git pull && make
-	)
-
-	cd ~/repo/generator-ysp && git co master && git pull && yarn install && yarn link
-
-	cd ~/repo/pages-builder && git pull
-
-	cd ~/repo/pages-tools && git pull
-
-	brew upgrade
-
-	(
-		npm i -g npm 
-		npm i -g bower
-	)
-
-	(
-		pip install --upgrade pip --user
-		pip install --upgrade awscli --user
-	)
-
-	cd ~/repo/yext-ce-tools && git pull
+	parallel=true
 fi
+
+# alpha and congo
+(
+	cd $YEXT && git co master && git pull && glock sync yext && make
+	cd $CONGO && git co master && git pull && glock sync congo && glock sync yext && make
+	cd $ALPHA && git co master && git pull && make
+) &
+if ! [ "$parallel" ]; then wait; fi
+
+# generator ysp
+cd ~/repo/generator-ysp && git co master && git pull && yarn install && yarn link &
+if ! [ "$parallel" ]; then wait; fi
+
+# pages builder
+cd ~/repo/pages-builder && git pull &
+if ! [ "$parallel" ]; then wait; fi
+
+# pages tools
+cd ~/repo/pages-tools && git pull &
+if ! [ "$parallel" ]; then wait; fi
+
+# Homebrew
+brew upgrade &
+if ! [ "$parallel" ]; then wait; fi
+
+# npm
+(
+	npm i -g npm 
+	npm i -g bower
+	npm install -g yo
+) &
+if ! [ "$parallel" ]; then wait; fi
+
+# Python
+(
+	pip install --upgrade pip --user
+	pip install --upgrade awscli --user
+) &
+if ! [ "$parallel" ]; then wait; fi
+
+
+
+# Keep this last so that updating doesn't change this script before it's finished
+# yext ce tools
+cd ~/repo/yext-ce-tools && git pull &
+wait
