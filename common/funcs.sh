825b8e4739397a650d105df7cc8e0557f56d2958
jronkin <jronkin@yext.com>
2019-05-06 22:05:18 -0400

    Add trash function

---
+++ common/funcs.sh
@@ -0,0 +1,19 @@
+trash() {
+	if [ "$1" ]
+	then
+		file="$1"
+
+		if [ ! -f "$file" ] && [ ! -d "$file" ]
+		then
+			echo "trash: ${file}: No such file or directory" >&2
+			exit 1
+		fi
+
+		if [[ ! "$file" = /* ]]
+		then
+			file="$(pwd)/$file"
+		fi
+
+		osascript -e "tell app \"Finder\" to delete POSIX file \"$file\"" 1>/dev/null
+	fi
+}
