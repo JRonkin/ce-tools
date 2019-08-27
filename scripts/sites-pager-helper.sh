4d7a7c2b0010712085e8dcc09748f0d45ffa83aa
jronkin <jronkin@yext.com>
2019-08-27 15:27:46 -0400

    sites-pager-helper: removed -- not needed now that pager uses sites pager

--- scripts/sites-pager-helper.sh
+++
@@ -1,8 +0,0 @@
-domain="$1"
-
-if [ ! "$domain" ]
-then
-  read -p "Site Domain: " domain
-fi
-
-sites-pager -m prod -d "${domain}" -e staging -l "$(dirname $(pwd))" —staticdirs 'src/.tmp' —templatedir 'src/templates'
