2f34e3320ea7c8d5705a7a731ac20868689887fb
jronkin <jronkin@yext.com>
2019-08-27 12:49:44 -0400

    trash: remove duplicate code in funcs.sh

--- common/funcs.sh
+++ common/funcs.sh
@@ -3,17 +3,5 @@ hash() {
 }
 
 trash() {
-  if [ "$1" ]
-  then
-    file="$1"
-
-    if [ ! -f "$file" ] && [ ! -d "$file" ]
-    then
-      echo "trash: ${file}: No such file or directory" >&2
-    else
-      pushd $(dirname "$file") > /dev/null
-      osascript -e "tell app \"Finder\" to delete POSIX file \"$(pwd)/$(basename "$file")\"" 1>/dev/null
-      popd > /dev/null
-    fi
-  fi
+  "$(dirname "${BASH_SOURCE[0]}")/../scripts/trash.sh" $@
 }
