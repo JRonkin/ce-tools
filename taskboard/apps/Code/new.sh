ce966a046013a113a4aafa720db5ba22b1ce6995
jronkin <jronkin@yext.com>
2019-08-29 18:23:26 -0400

    taskboard: finish VSCode new script

--- taskboard/apps/Code/new.sh
+++ taskboard/apps/Code/new.sh
@@ -7,4 +7,19 @@ size="$5"
 [ "$position" ] || position='279, 23'
 [ "$size" ] || size='1331, 1027'
 
-code "$folder"
+selector="window whose name is \"${jiranum}\""
+
+code "$folder" && sleep 2 &&
+  osascript -e "
+    tell app \"System Events\"
+      tell process \"Code\"
+        set timer to 0
+        repeat until the length of (get every ${selector}) > 0 or timer > 15
+          delay 0.5
+          set timer to timer + 0.5
+        end repeat
+        set the position of every ${selector} to {${position}}
+        set the size of every ${selector} to {${size}}
+      end tell
+    end tell
+  " &
