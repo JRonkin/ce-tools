cedb98ecd918c17b1dc3a7e2eb00cb6f81e1cac3
jronkin <jronkin@yext.com>
2019-09-23 22:40:32 -0400

    taskboard: change default Terminal bounds

--- taskboard/apps/Terminal/new.sh
+++ taskboard/apps/Terminal/new.sh
@@ -4,14 +4,16 @@ repo="$3"
 position="$4"
 size="$5"
 
-[ "$position" ] || position='0, 347'
-[ "$size" ] || size='571, 353'
+[ "$position" ] || position='0, 373'
+[ "$size" ] || size='571, 339'
+
+bounds="${position}, $(( ${position%,*} + ${size%,*} )), $(( ${position#*,} + ${size#*,} ))"
 
 osascript -e "
   tell app \"Terminal\"
     do script \"J=${jiranum}; cd ${folder}; source '$(pwd)/startup-script.sh' '${repo}'\"
-    set the custom title of the front window to \"${jiranum}\"
-    set the position of the front window whose name contains \"${jiranum} — \" to {${position}}
-    set the size of the front window whose name contains \"${jiranum} — \" to {${size}}
+    set new_window to the front window
+    set the custom title of new_window to \"${jiranum}\"
+    set the bounds of new_window to {${bounds}}
   end tell
 " &
