3211fa185f189162f867c43c93150fb717e6fc04
JRonkin <41006960+JRonkin@users.noreply.github.com>
2021-08-31 21:21:37 -0400

    allow trash script to take any number of args

--- scripts/trash.sh
+++ scripts/trash.sh
@@ -1,13 +1,15 @@
-if [ "$1" ]
-then
+while [ "$1" ]
+do
   file="$1"
 
   if [ ! -f "$file" ] && [ ! -d "$file" ]
   then
     echo "trash: ${file}: No such file or directory" >&2
   else
-    pushd $(dirname "$file") >/dev/null
-    osascript -e "tell app \"Finder\" to delete POSIX file \"$(pwd)/$(basename "$file")\"" 1>/dev/null
-    popd >/dev/null
+    osascript -e "tell app \"Finder\" to delete POSIX file \"$(realpath "$file")\"" 1>/dev/null &
   fi
-fi
+
+  shift
+done
+
+wait
